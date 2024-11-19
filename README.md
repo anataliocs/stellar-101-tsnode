5829603840fa1dbdaace7652e2a17afdb66ea70c
Chris Anatalio <chris.anatalio@stellar.org>
2024-11-19 08:24:49 -0800

    Update docs

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Install deps:
 npm install
 ```
 
-### Quest 1
+## Quest 1:  Create Account
 
 https://quest.stellar.org/learn/series/1/quest/1
 
@@ -19,10 +19,19 @@ The script to create an account.
 Parameters:
 - SECRET_KEY (required)
 
+Command:
 ```
 npx ts-node quest1.ts SECRET_KEY
 ```
 
+*Additional Details on Stellar Accounts*
+- [Stellar Accounts](https://developers.stellar.org/docs/learn/fundamentals/stellar-data-structures/accounts)
+- [SDK Docs Account](https://stellar.github.io/js-stellar-sdk/Account.html)
+- [Stellar XDR Data Format](https://developers.stellar.org/docs/learn/encyclopedia/data-format/xdr)
+
+----
+
 ## Support
 
 - [Stellar Discord](https://discord.com/invite/zVYdY3ktTn)
+- [Stellar Docs](https://developers.stellar.org/)
--- quest1.ts
+++ quest1.ts
@@ -2,25 +2,32 @@ import { BASE_FEE, Horizon, Keypair, Networks, Operation, Transaction, Transacti
 
 module.exports = (async function () {
 
+    // Ensure secret key is passed in
     if (!process.argv[2]) {
         console.error(`You must provide a SECRET_KEY as a parameter \n`);
         return;
     }
 
+    // Generate keypair from passed in secret key
     const secretkey: string = process.argv[2];
     const questKeypair = Keypair.fromSecret(secretkey);
+
     const newKeypair = Keypair.random()
 
+    // Load up testnet Stellar account of passed in secret key
     const server: Horizon.Server = new Horizon.Server('https://horizon-testnet.stellar.org');
     const questAccount = await server.loadAccount(questKeypair.publicKey());
 
+    // Build a Stellar Transaction to execute create account operation
+    // That will create a account on testnet with 1000 test XLM
+    // 1000 XLM will be debited from the Quest Keypair passed in as a secret key
     let transaction: Transaction = new TransactionBuilder(
         questAccount, {
         fee: BASE_FEE,
         networkPassphrase: Networks.TESTNET
     }).addOperation(Operation.createAccount({
         destination: newKeypair.publicKey(),
-        startingBalance: "1000" // You can make this any amount you want (as long as you have the funds!)
+        startingBalance: "1000"
     }))
         .setTimeout(30)
         .build();
@@ -32,7 +39,8 @@ module.exports = (async function () {
 
     try {
         let res = await server.submitTransaction(transaction)
-        console.log(`Transaction Successful! Hash: ${res.hash}`)
+        console.log("Stellar Account Created! Transaction Details: ");
+        console.log(`https://stellar.expert/explorer/testnet/tx/${res.hash}`);
     } catch (error) {
         console.log(`${error}. More details:\n${JSON.stringify(error.response.data.extras, null, 2)}`)
     }
